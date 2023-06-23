880da43c17efdab524d8bc9db4de0a2fa70fc18d
Vladimir <vermakov410@gmail.com>
2023-06-23 19:56:18 +0300

    Задача 3

--- Program.cs
+++ Program.cs
@@ -52,23 +52,80 @@
 //  17 -> такого числа в массиве нет
 
 
+// Console.WriteLine("Введите число сторок массива");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число столбцов массива");
+// int colums = Convert.ToInt32(Console.ReadLine());
+// int[,] numbers = new int[rows, colums];
+
+// Console.WriteLine("Введите индекс стороки");
+// int rows1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите индекс столбца");
+// int colums1 = Convert.ToInt32(Console.ReadLine());
+// FillArray(numbers);
+// PrintArray(numbers);
+
+// if (rows1 < numbers.GetLength(0) && colums1 < numbers.GetLength(1)) Console.WriteLine(numbers[rows1, colums1]);
+// else Console.WriteLine($"{rows1} {colums1} -> такого числа в массиве нет");
+
+// void FillArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(1, 10);
+//         }
+//     }
+// }
+
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
 Console.WriteLine("Введите число сторок массива");
 int rows = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите число столбцов массива");
 int colums = Convert.ToInt32(Console.ReadLine());
 int[,] numbers = new int[rows, colums];
-
-Console.WriteLine("Введите индекс стороки");
-int rows1 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите индекс столбца");
-int colums1 = Convert.ToInt32(Console.ReadLine());
 FillArray(numbers);
 PrintArray(numbers);
 
-if (rows1 < numbers.GetLength(0) && colums1 < numbers.GetLength(1)) Console.WriteLine(numbers[rows1, colums1]);
-else Console.WriteLine($"{rows1} {colums1} -> такого числа в массиве нет");
+double[] avgNumbers = new double[numbers.GetLength(1)];
+
+for (int i = 0; i < numbers.GetLength(1); i++)
+{
+    double result = 0.0;
+    for (int j = 0; j < numbers.GetLength(0); j++)
+    {
+        result += numbers[j, i];
+    }
+    avgNumbers[i] = result / numbers.GetLength(0);
+}
+
+Console.WriteLine("Среднее арифметическое каждого столбца:");
+PrintArrayAvg(avgNumbers);
 
 void FillArray(int[,] array)
 {
@@ -94,6 +151,13 @@ void PrintArray(int[,] array)
     Console.WriteLine();
 }
 
-
+void PrintArrayAvg(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
 
 
